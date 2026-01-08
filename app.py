3efcd2eedba7e7f08da2a82cf090b6e81e3cc121
Prarthana <prarthanajoshi6424@gmail.com>
2026-01-08 12:51:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import streamlit as st
-import numpy as np
-from PIL import Image
-import cv2
 from ultralytics import YOLO
+from PIL import Image
+import numpy as np
 
 # --- PAGE CONFIG ---
 st.set_page_config(page_title="YOLO11 Object Detection", page_icon="🎯", layout="wide")
